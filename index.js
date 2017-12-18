ab5743d9de82e860868eeb9301fac313befacb3a
Marcos Mendes <marcos.mendes@live.com>
2017-12-18 02:43:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var express = require('express');
 var Feed = require('rss-to-json');
 var app = express();
 
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	next();
+});
+
 app.get('/list', (req, res) => {
 	var username = req.param('username');
 
